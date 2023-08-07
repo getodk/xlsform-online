6ac6d5135e701ea17e1139812aebf34bd23ec538
Yaw Anokwa <yanokwa@gmail.com>
2023-08-07 14:27:53 -0700

    Remove unused endpoint

--- xlsform_app/urls.py
+++ xlsform_app/urls.py
@@ -3,7 +3,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
-    url(r'^json_workbook/', views.json_workbook),
     url(r'^$', views.index),
     url(r'^downloads/(?P<path>.*)$', views.serve_file),
 ]
