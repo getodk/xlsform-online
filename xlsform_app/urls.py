6152e5b57ac43f6a32cb379b7a4eb6a683bd300d
Yaw Anokwa <yanokwa@gmail.com>
2024-12-18 09:30:43 -0800

    Fix Django vuln

--- xlsform_app/urls.py
+++ xlsform_app/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^downloads/(?P<path>.*)$', views.serve_file),
-    url(r'^api/xlsform$', views.api_xlsform),
+    re_path(r'^$', views.index),
+    re_path(r'^downloads/(?P<path>.*)$', views.serve_file),
+    re_path(r'^api/xlsform$', views.api_xlsform),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
